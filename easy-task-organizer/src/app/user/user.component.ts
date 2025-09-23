6c05b4a274cbe51678dd13f624487a6edda7c6ca
Yevgen Konovalov <yevgen.konovalov@stryker.com>
2025-09-23 17:56:24 +0200

    Using Signak Inputs

--- easy-task-organizer/src/app/user/user.component.ts
+++ easy-task-organizer/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!:string;
-
-  get imagePath(){
-    return 'assets/users/' + this.avatar;
-  }
+  avatar = input.required<string>();
+  name = input.required<string>();
+  imagePath = computed(() => 'assets/users/' + this.avatar());
 
   onSelectUser()
   {
