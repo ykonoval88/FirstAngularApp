4339af38059b1c1884172383b44ff441556086f3
Yevgen Konovalov <yevgen.konovalov@stryker.com>
2025-09-26 14:54:50 +0200

    Outputting Conditional Content

--- easy-task-organizer/src/app/app.component.ts
+++ easy-task-organizer/src/app/app.component.ts
@@ -15,7 +15,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = this.users[0].id;
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
