707a45108cc7ba45b6f0d63f66052750b38ca86f
Yevgen Konovalov <yevgen.konovalov@stryker.com>
2025-09-26 16:54:33 +0200

    Component with output. Add models.

--- easy-task-organizer/src/app/tasks/tasks.component.ts
+++ easy-task-organizer/src/app/tasks/tasks.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { DUMMY_TASKS } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name: string | undefined;
+  @Input( {required : true} ) userId!: string;
+  @Input( {required : true} ) name!: string;
+  tasks = DUMMY_TASKS;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
--- easy-task-organizer/src/app/user/user.component.ts
+++ easy-task-organizer/src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// };
-
-interface User{
-  id: string;
-  avatar: string;
-  name: string;
-};
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
