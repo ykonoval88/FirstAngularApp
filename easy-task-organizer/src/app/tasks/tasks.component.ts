672b253bf7bf3c07685428a0e60767e53710b392
Yevgen Konovalov <yevgen.konovalov@stryker.com>
2025-09-30 13:17:41 +0200

    Add new-task compnent and event to hide dialog.

--- easy-task-organizer/src/app/tasks/tasks.component.ts
+++ easy-task-organizer/src/app/tasks/tasks.component.ts
@@ -1,17 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from '../dummy-tasks';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input( {required : true} ) userId!: string;
   @Input( {required : true} ) name!: string;
+  isAddingTask = false;
+
   tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
@@ -20,5 +23,13 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
