ca234a3e92c02d7350e8c4c75baf3b013938a3db
Yevgen Konovalov <yevgen.konovalov@stryker.com>
2025-10-03 15:14:17 +0200

    Content Projection with ng-content.

--- easy-task-organizer/src/app/tasks/task/task.component.ts
+++ easy-task-organizer/src/app/tasks/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
