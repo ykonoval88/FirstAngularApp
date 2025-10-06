dd910a098fa6d49dab5436994761ba69f76dd051
Yevgen Konovalov <yevgen.konovalov@stryker.com>
2025-10-06 15:34:23 +0200

    Using LocalStorage for Data Storage.

--- easy-task-organizer/src/app/tasks/tasks.service.ts
+++ easy-task-organizer/src/app/tasks/tasks.service.ts
@@ -6,6 +6,14 @@ import { NewTaskData } from './new-task/new-task.model';
 export class TasksService {
     private tasks = DUMMY_TASKS;
 
+    constructor(){
+        const tasks = localStorage.getItem('tasks');
+
+        if (tasks) {
+            this.tasks = JSON.parse(tasks);
+        }
+    }
+
     getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId);
     }
@@ -19,9 +27,15 @@ export class TasksService {
             dueDate: taskData.date
         };
         this.tasks.push(newTask);
+        this.saveTasks();
     }
 
     removeTask(id: string) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
+        this.saveTasks();
+    }
+
+    private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
