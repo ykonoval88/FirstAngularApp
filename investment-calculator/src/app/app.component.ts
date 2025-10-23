4bb524fcc2d6691503f004a5549d3e3ca7eb9d36
Yevgen Konovalov <yevgen.konovalov@stryker.com>
2025-10-23 18:03:31 +0200

    Using data model.

--- investment-calculator/src/app/app.component.ts
+++ investment-calculator/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestmentInput } from "./investment-input.model";
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,7 @@ import { UserInputComponent } from "./user-input/user-input.component";
   templateUrl: './app.component.html',
 })
 export class AppComponent { 
-  calculateInvestmentResults(data: {
-    initialInvestment: number;
-    annualInvestment: number;
-    expectedReturn: number;
-    duration: number;
-  }) {
+  calculateInvestmentResults(data: InvestmentInput) {
   const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
   const annualData = [];
   let investmentValue = initialInvestment;
--- investment-calculator/src/app/user-input/user-input.component.ts
+++ investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,12 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<{
-    initialInvestment: number;
-    annualInvestment: number;
-    expectedReturn: number;
-    duration: number;
-  }>();
+  @Output() calculate = new EventEmitter<InvestmentInput>();
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
