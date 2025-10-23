79dd6688f0e04214d053b07c866ee17d46c3dcad
Yevgen Konovalov <yevgen.konovalov@stryker.com>
2025-10-23 11:19:26 +0200

    Creating and using Shared Modules.

--- easy-task-organizer/src/app/app.module.ts
+++ easy-task-organizer/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
 import { TaskComponent } from "./tasks/task/task.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -16,12 +16,11 @@ import { FormsModule } from '@angular/forms';
         AppComponent,
         HeaderComponent,
         UserComponent,
-        CardComponent,
         TasksComponent,
         TaskComponent,
         NewTaskComponent
     ], // Use declarations for non-standalone components
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule] // Use standalone components here
+    imports: [BrowserModule, FormsModule, SharedModule ] // Use standalone components here
 })
 export class AppModule { }
